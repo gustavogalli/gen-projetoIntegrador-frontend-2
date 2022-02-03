496ecaf132f98d5dc77b1a3cda428dc4f31bd199
Gustavo Galli <gustavosgalli@gmail.com>
2022-02-03 13:39:08 -0300

    @gustavogalli [Task: Refatorando Entrar e Cadastrar]

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -14,16 +14,16 @@ export class AuthService {
     private http: HttpClient
   ) { }
 
-  entrar(usuarioLogin: UsuarioLogin): Observable<UsuarioLogin>{ //observable GARANTE o tipo que será passado
-    return this.http.post<UsuarioLogin>("https://gen-ti.herokuapp.com/usuarios/logar", usuarioLogin) // salva no caminho em questão o parâmetro passado (usuarioLogin)
+  entrar(usuarioLogin: UsuarioLogin): Observable<UsuarioLogin>{
+    return this.http.post<UsuarioLogin>("https://gen-ti.herokuapp.com/usuarios/logar", usuarioLogin)
   }
 
   cadastrar(usuario: Usuario): Observable<Usuario>{
     return this.http.post<Usuario>("https://gen-ti.herokuapp.com/usuarios/cadastrar", usuario)
   }
 
-  atualizar(usuario: Usuario):Observable<Usuario>{
-    return this.http.put<Usuario>('https://gen-ti.herokuapp.com/usuarios/atualizar',usuario)
+  atualizar(usuario :Usuario):Observable<Usuario>{
+    return this.http.put<Usuario>("https://gen-ti.herokuapp.com/usuarios/atualizar", usuario)
   }
 
   getByIdUsuario(id: number): Observable<Usuario>{
